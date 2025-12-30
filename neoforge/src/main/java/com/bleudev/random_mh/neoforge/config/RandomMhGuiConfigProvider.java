a13fb5301572cdd465b0e5c50c7a4e4347acebd9
bleudev <aitiiigg1@gmail.com>
2025-12-30 23:00:47 +0300

    done client side config

--- neoforge/src/main/java/com/bleudev/random_mh/neoforge/config/RandomMhGuiConfigProvider.java
+++ neoforge/src/main/java/com/bleudev/random_mh/neoforge/config/RandomMhGuiConfigProvider.java
@@ -2,6 +2,7 @@ package com.bleudev.random_mh.neoforge.config;
 
 import dev.isxander.yacl3.api.*;
 import dev.isxander.yacl3.api.controller.IntegerFieldControllerBuilder;
+import dev.isxander.yacl3.api.controller.TickBoxControllerBuilder;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
@@ -20,6 +21,22 @@ public class RandomMhGuiConfigProvider {
                     .description(OptionDescription.of(Component.literal("How much speedrunners will be")))
                     .binding(Binding.generic(getDefaultConfig().speedrunnersCount(), getConfig()::speedrunnersCount, RandomMhConfigProvider::setSpeedrunnersCount))
                     .controller(o -> IntegerFieldControllerBuilder.create(o).min(0))
+                    .build())
+                .group(OptionGroup.createBuilder()
+                    .name(Component.literal("Random"))
+                    .description(OptionDescription.of(Component.literal("Random features")))
+                    .option(Option.<Boolean>createBuilder()
+                        .name(Component.literal("Should randomise speedrunners"))
+                        .description(OptionDescription.of(Component.literal("Should mod randomise speedrunners every specified time")))
+                        .binding(Binding.generic(getDefaultConfig().shouldRandomiseSpeedrunners(), getConfig()::shouldRandomiseSpeedrunners, RandomMhConfigProvider::setShouldRandomiseSpeedrunners))
+                        .controller(TickBoxControllerBuilder::create)
+                        .build())
+                    .option(Option.<Integer>createBuilder()
+                        .name(Component.literal("Randomisation time (ticks)"))
+                        .description(OptionDescription.of(Component.literal("After how much time mod should randomise speedrunners (in ticks, 1 second - 20 ticks)")))
+                        .binding(Binding.generic(getDefaultConfig().randomisationTime(), getConfig()::randomisationTime, RandomMhConfigProvider::setRandomisationTime))
+                        .controller(o -> IntegerFieldControllerBuilder.create(o).min(0))
+                        .build())
                     .build())
                 .build())
             .build().generateScreen(parent);
